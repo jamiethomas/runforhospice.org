9003ec5aeb4d5048a6a3af396db237fc802a5879
Jamie Thomas <james.b.thomas@gmail.com>
2016-05-10 20:25:28 -0400

    Update contact us form and add FAQs

--- js/custom-f6161891.js
+++
@@ -1,6 +0,0 @@
-/*!
- * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-$(function(){$("a.page-scroll").bind("click",function(o){var t=$(this);$("html, body").stop().animate({scrollTop:$(t.attr("href")).offset().top},1500,"easeInOutExpo"),o.preventDefault()})}),$("body").scrollspy({target:".navbar-fixed-top"}),$(".navbar-collapse ul li a").click(function(){$(".navbar-toggle:visible").click()}),$("#DateCountdown").TimeCircles({animation:"smooth",bg_width:1.2,fg_width:.1,circle_bg_color:"#60686F",time:{Days:{text:"Days",color:"#FFCC66",show:!0},Hours:{text:"Hours",color:"#99CCFF",show:!0},Minutes:{text:"Minutes",color:"#BBFFBB",show:!0},Seconds:{text:"Seconds",color:"#FF9999",show:!0}}});
