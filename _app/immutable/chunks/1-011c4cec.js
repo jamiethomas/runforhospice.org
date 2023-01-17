85c006d2ba76c6ab366281ef42399cf138a34660
Jamie Thomas <james.b.thomas@gmail.com>
2023-01-17 18:12:12 -0500

    Update for Sveltekit 1.0

---
+++ _app/immutable/chunks/1-011c4cec.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-4b6b2569.js";export{t as component};
---
+++ _app/immutable/chunks/_layout-0b50e484.js
@@ -0,0 +1,1 @@
+async function e(){return{}}const t=!0,o=Object.freeze(Object.defineProperty({__proto__:null,load:e,prerender:t},Symbol.toStringTag,{value:"Module"}));export{o as _,e as l,t as p};
