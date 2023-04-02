edd767173931342299a47e545af8d788fbb5a0bb
Jamie Thomas <james.b.thomas@gmail.com>
2023-04-02 10:10:46 -0400

    Update sponsors

---
+++ _app/immutable/chunks/2-6fe988cc.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-144132d4.js";export{t as component};
--- _app/immutable/chunks/2-c2e3dc0e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-62edcfaa.js";export{t as component};
