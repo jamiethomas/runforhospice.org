b787e333a6bfbe2e397496046b83300dc25e13dd
Jamie Thomas <james.b.thomas@gmail.com>
2023-04-13 20:49:08 -0400

    Update memory wall for 2023

---
+++ _app/immutable/chunks/2-bcebde53.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-1699a73c.js";export{t as component};
--- _app/immutable/chunks/2-c4983d52.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-7a1a9b9d.js";export{t as component};
