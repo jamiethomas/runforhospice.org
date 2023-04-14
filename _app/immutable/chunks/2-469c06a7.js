2ece55fce01589096c932fd9200272fd5c55f3cf
Jamie Thomas <james.b.thomas@gmail.com>
2023-04-14 15:44:26 -0400

    Add Bozick as a sponsor

--- _app/immutable/chunks/2-469c06a7.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-548c0284.js";export{t as component};
---
+++ _app/immutable/chunks/2-d930ee0a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-51e75a03.js";export{t as component};
